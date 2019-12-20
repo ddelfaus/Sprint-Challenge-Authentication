ebe5727c6bb7bcb031a031c5ba14f81cd926f937
ddelfaus <ddelfaus@gmail.com>
2019-12-20 11:32:32 -0500

    login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,8 +23,48 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+      
+        const token = signToken(user);
 
-  
+      
+        res.status(200).json({
+          token, 
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
 });
+
+
+
+function signToken(user) {
+  const payload = {
+    username: user.username,
+    
+  };
+
+  const secret = process.env.JWT_SECRET || "key";
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options); 
+}
+
+
+
+
 
 module.exports = router;
